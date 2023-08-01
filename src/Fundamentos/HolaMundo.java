64ee1477b40f50d523f3dd7d92df0f7416da7d8c
GustavMG <magg950506@gmail.com>
2023-08-01 11:05:23 -0600

    Se crea una clase para imprimir en consola un Hola Mundo

---
+++ src/Fundamentos/HolaMundo.java
@@ -0,0 +1,7 @@
+package Fundamentos;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo, estamos haciendo nuestros repasos de Java");
+    }
+}
