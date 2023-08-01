4ce19c8101e962008342c3f0a2c958100c5be15d
GustavMG <magg950506@gmail.com>
2023-08-01 13:56:08 -0600

    Se trabaja con Constantes (final)

---
+++ src/Fundamentos/OperadoresYExpresiones/OperadoresAritmeticos.java
@@ -0,0 +1,20 @@
+package Fundamentos.OperadoresYExpresiones;
+
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+        //Aritmeticos
+        int numero1 = 10;
+        int numero2 = 20;
+
+        int resultadoSuma = numero1 + numero2;
+        int resultadoResta = numero2 - numero1;
+        int resultadoMultiplicacion = numero1 * numero2;
+        int resultadoDivision = numero2 / numero1;
+
+        System.out.println("El resultado de la suma es: " + resultadoSuma);
+        System.out.println("El resultado de la resta es: " + resultadoResta);
+        System.out.println("El resultado de la multiplicación es: " + resultadoMultiplicacion);
+        System.out.println("El resultado de la división es: " + resultadoDivision);
+
+    }
+}
