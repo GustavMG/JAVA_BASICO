97080d329c524158d0b4a594c8135cf3069cac6c
GustavMG <magg950506@gmail.com>
2023-08-02 13:56:31 -0600

    Se comienza a trabajar con Programación Orientada a Objetos (POO)

---
+++ src/POO/Clases/Main.java
@@ -0,0 +1,12 @@
+package POO.Clases;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Comenzamos con POO trabajando con clases");
+        /**
+         * En POO, una clase es una plantilla o un plano para crear objetos.
+         * Define los atributos (variables) y métodos (funciones) que tendrán
+         * los objetos creados a partir de ella.
+         */
+    }
+}
