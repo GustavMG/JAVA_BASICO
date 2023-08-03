79bb5e1f35b02914e9b14e8589000085d284cd9f
GustavMG <magg950506@gmail.com>
2023-08-03 01:05:54 -0600

    Se trabaja con los primeros objetos de la clase Vehiculo

--- src/POO/Clases/Main.java
+++ src/POO/Clases/Main.java
@@ -8,5 +8,26 @@ public class Main {
          * Define los atributos (variables) y métodos (funciones) que tendrán
          * los objetos creados a partir de ella.
          */
+
+        /**
+         * Clase identificador = new Clase();
+         * Un poco mas desglosado
+         *         NombreDeLaClase nombreDelObjeto = new NombreDeLaClase(parametrosDelConstructor);
+         */
+
+        //Primera creación de objeto con el constructor vacio de la clase
+        Vehiculo toyotaPrius = new Vehiculo();
+
+        //Creación de un objeto CON parámetros
+        Vehiculo fordMondeo = new Vehiculo("Ford", "Mondeo", 2.1, 2010, false);
+
+        //Aqui podemos acceder a los atributos de la clase pq NO estan protegidos con su scoop
+        System.out.println(fordMondeo.fabricante);
+        System.out.println(fordMondeo.anio);
+
+        //Aqui accedemos a los métodos del objeto
+        System.out.println(fordMondeo.velocidad);   //Velocidad = 0
+        fordMondeo.acelerar(50);
+        System.out.println(fordMondeo.velocidad);   //Velocidad = 50
     }
 }
