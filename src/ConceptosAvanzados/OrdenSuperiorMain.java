c85ca803319625f5af646424173617f111ec111b
GustavMG <magg950506@gmail.com>
2023-08-27 20:17:06 -0600

    Trabajamos con funciones lambda, uso de map, filter y reduce ademaa de comparación de escritura de código de forma declarativo e imperativa

--- src/ConceptosAvanzados/OrdenSuperiorMain.java
+++ src/ConceptosAvanzados/OrdenSuperiorMain.java
@@ -1,6 +1,8 @@
 package ConceptosAvanzados;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class OrdenSuperiorMain {
@@ -20,8 +22,11 @@ public class OrdenSuperiorMain {
         nombres.add("Juanito");
         nombres.add("Miguelito");
         nombres.add("Panchito");
+        nombres.add("Chucho");
+        nombres.add("Pepito");
+        nombres.add("Fulanito");
 
-        System.out.println("Usando un for each convencional");
+        System.out.println("1. Usando un for each convencional");
         for (String nombre : nombres){
             System.out.println(nombre);
         }
@@ -33,7 +38,7 @@ public class OrdenSuperiorMain {
          * representa los elementos contenidos en esa colección. Este flujo permite realizar operaciones de
          * procesamiento, filtrado, transformación y más, de una manera más funcional y declarativa.
          */
-        System.out.println("Usando una función lambda");
+        System.out.println("2. Usando una función lambda");
         nombres.stream().forEach(x -> System.out.println(x));
         System.out.println("");
 
@@ -56,9 +61,68 @@ public class OrdenSuperiorMain {
          *                 });
          *
          */
-        System.out.println("Usando el stream con un foreach y el método toUpperCase()");
+        System.out.println("3. Usando el stream con un foreach y el método toUpperCase()");
         Stream<String> nombresStream= nombres.stream().map((x) -> "Hola, " + x.toUpperCase());
         nombresStream.forEach(x -> System.out.println(x));
         System.out.println("");
+
+        /**
+         * Existen 3 operaciones que son comunmente utilizadas con los Stream y son el map, filter y reduce que
+         * se utilizan ampliamente para transformar y procesar datos de manera declarativa y eficiente.
+         *
+         * Aqui de principio esramos usando la función .map para iterar sobre todos los elementos y convertirlos
+         * en mayusculas y posteriormente usamos .filter, quien recibe los parametro de la función map como entrada,
+         * para filtrar los elementos que comienzan con "P", podria ser cualquier otra condición que sea compatible
+         * con nuestro tipo elemento, recordemos que las clases envolventes como String, Integer, Double, etc, tienen
+         * sus propios métodos.
+         */
+        System.out.println("4. Usando map & filter");
+        Stream<String> nombresFiltrados= nombres.stream()
+                .map((x) -> x.toUpperCase())
+                .filter((x) -> x.startsWith("P"));
+        nombresFiltrados.forEach(x -> System.out.println(x));
+        System.out.println("");
+
+        System.out.println("5. Filtro con enteros");
+        int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        IntStream numerosStream = Arrays.stream(numeros);
+        IntStream numerosPares = numerosStream.filter((x) -> x % 2 == 0);
+        /**
+         * @Nota: Recordemos que los Stream SOLO SE PUEDEN CONSUMIR una vez, estamos consumiendo en un principio
+         * al filtrar los elementos pares por lo que debemos crear otra variable del mismo tipo InStream para
+         * poder trabajar y hacer otra iteración que en este caso es para imprimir los datos en consola.
+         */
+        numerosPares.forEach(x -> System.out.println(x));
+
+        System.out.println("6. Usando filter & reduce");
+        int[] numeros2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        IntStream numerosStream2 = Arrays.stream(numeros);
+//        Integer numerosReduce = numerosStream2.filter((x) -> x % 2 == 0).reduce(0, (x, y) -> {
+//            System.out.println("El valor de x es: " + x + " y el valor de y es: " + y);
+//            return x + y;
+//        });
+//        Integer numerosReduce = numerosStream2
+//                .map((x) -> x * 2)
+//                .filter((x) -> x % 2 == 0)
+//                .reduce(0, (x, y) -> {
+//            System.out.println("El valor de x es: " + x + " y el valor de y es: " + y);
+//            return x + y;
+//        });
+        //Forma Funcional o declarativa
+        Integer numerosReduce = numerosStream2
+                .map((x) -> x * 2)
+                .filter((x) -> x % 2 == 0)
+                .reduce(0, (x, y) -> x + y);
+
+        //Forma imperativa
+        int sumaTotal = 0;
+        for(int numero : numeros2){
+            numero = numero * 2;
+            if(numero % 2 != 0){
+                continue;
+            }
+            sumaTotal += numero;
+        }
+        System.out.println("Mi suma de pares con numerosReduce es: " + numerosReduce + " y con for: " + sumaTotal);
     }
 }
